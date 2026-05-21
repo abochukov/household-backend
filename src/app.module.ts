1f13219ae3ecf607eec5ac3af9a7902b82273ea4
Atanas Evgeniev Bochukov <abochukov@worldbankgroup.org>
2026-05-21 14:04:58 +0300

    Implement sms sending

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,10 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AuthInterceptor } from './common/interceptors/auth.interceptor';
 import { AddressModule } from './address/address.module';
 import { PropertyModule } from './property/property.module';
+import { SmsModule } from './sms/sms.module';
 
 @Module({
-  imports: [AuthModule, MeModule, AddressModule, PropertyModule],
+  imports: [AuthModule, MeModule, AddressModule, PropertyModule, SmsModule],
   controllers: [AppController],
   providers: [
     AppService,
